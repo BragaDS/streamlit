25d617a6562b95ce645295ffb8a0f0d220ceaa9e
Ramon Braga <79801558+BragaDS@users.noreply.github.com>
2024-01-01 23:06:41 -0300

    Update 1_💻Homepage.py

--- 1_💻Homepage.py
+++ 1_💻Homepage.py
@@ -2,6 +2,16 @@ import streamlit as st
 from PIL import Image
 import os
 
+st.markdown("""
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FMHPECVJSG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-FMHPECVJSG');
+</script>
+""", unsafe_allow_html=True)
+
 # Obtém o diretório do script
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
